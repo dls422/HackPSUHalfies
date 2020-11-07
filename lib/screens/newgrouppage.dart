199c56b9b0bf78740ee0fac40262385aac030b7a
scatalfo <scatalfo@gmail.com>
2020-11-07 18:30:46 -0500

    Progress on group creation page
    we can now read for the database and get all of the names that are inside and put them into a list view. very cool

--- lib/screens/newgrouppage.dart
+++ lib/screens/newgrouppage.dart
@@ -2,16 +2,41 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_list.dart';
 import 'package:flutter/material.dart';
 import 'package:halfsies/screens/homepage.dart';
-
 class NewGroupPage extends StatefulWidget {
+
   @override
   _NewGroupPageState createState() => _NewGroupPageState();
+
 }
 
 class _NewGroupPageState extends State<NewGroupPage> {
   String groupName;
   var data = new List();
+  var names;
+  List getNames()
+  {
+    var names = new List();
+    var db = FirebaseDatabase.instance.reference();
+    db.once().then((DataSnapshot snapshot){
+      Map<dynamic, dynamic> values = snapshot.value;
+      values.forEach((key,values) {
+        print(values["Display Name"]);
+        if(values["Display Name"]!= null) {
+          setState(() {
+            names.add(values["Display Name"]);
+          });
+        }
+      });
+    });
+    return names;
+  }
 
+  @override
+  void initState()
+  {
+    names = getNames();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +47,7 @@ class _NewGroupPageState extends State<NewGroupPage> {
           children: [
             TextField(
               decoration: new InputDecoration(hintText: "Group name"),
-              onChanged: (value) {
+              onChanged: (value) async {
                 groupName = value;
                 if(data.length == 0)
                   {
@@ -46,8 +71,26 @@ class _NewGroupPageState extends State<NewGroupPage> {
                     Navigator.pop(context, data);
                   }
                 }),
+            Expanded(
+              child:
+              ListView.builder(
+                padding: const EdgeInsets.all(8),
+                itemCount: names.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                      height: 50,
+                      child: Center(child: Text("${names[index]}"))
+                  );
+                },
+
+
+              ),
+
+            )
+
           ],
 
         ));
   }
 }
+
