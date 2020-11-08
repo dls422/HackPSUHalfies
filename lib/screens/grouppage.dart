38f6922b6f59e91beefd470c19f1461b5c58e48b
Matthew Broadbent <53876387+mattjb01@users.noreply.github.com>
2020-11-08 14:59:47 -0500

    Forgot to rebuild UI for add payments

--- lib/screens/grouppage.dart
+++ lib/screens/grouppage.dart
@@ -103,6 +103,7 @@ class GroupPage extends StatelessWidget {
         break;
       case 'Add payments':
         editMembers = false;
+        (context1 as Element).markNeedsBuild();
         break;
       case 'Delete group':
         FirebaseDatabase.instance.reference().child(FirebaseAuth.instance.currentUser.uid).child("Groups").child(group.name).remove();
